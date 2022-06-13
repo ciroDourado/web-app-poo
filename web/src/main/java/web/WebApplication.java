6e90cb4fda4a28fb4d7f25ac591553b9bae2c1a3
ciroDourado <ciro.brz@gmail.com>
2022-06-12 21:07:23 -0300

    (refactor) removendo teste de consumo da api

--- web/src/main/java/web/WebApplication.java
+++ web/src/main/java/web/WebApplication.java
@@ -3,28 +3,10 @@ package web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import web.model.Curso;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-
 @SpringBootApplication
 public class WebApplication {
-
-	private static final Logger log = LoggerFactory.getLogger(WebApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
-
-	@Bean
-  CommandLineRunner execute() {
-		RestTemplate consumer = new RestTemplate();
-		String       url      = "http://localhost:8080/cursos/1";
-		Curso        curso    = consumer.getForObject(url, Curso.class);
-    return args -> { log.info(curso.toString()); };
-  }
 }
