bc49b8c83069441168180731e8b0bbb0c023bdf4
ciroDourado <ciro.brz@gmail.com>
2022-06-17 08:49:13 -0300

    (wip) feat: criando controller para retornar o component

--- component-api/src/main/java/componentapi/http/controller/curso/IndexRowController.java
+++ component-api/src/main/java/componentapi/http/controller/curso/IndexRowController.java
@@ -1,54 +1,26 @@
-package api.http.controller.curso;
+package componentapi.http.controller.curso;
 
-import api.model.curso.IndexRow;
+import componentapi.model.entity.Curso;
+import componentapi.model.component.curso.IndexRow;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value="curso")
 public class IndexRowController {
 
   IndexRowController() {}
 
-  // @PostMapping("/cursos")
-  // Curso newCurso(@RequestBody Curso newCurso) {
-  //   return repository.save(newCurso);
-  // }
-
-  // // Single item
-
-  // @GetMapping("/cursos/{id}")
-  // Curso one(@PathVariable Long id) {
-
-  //   return repository
-	// 		.findById(id)
-  //     .orElseThrow(() -> new CursoNotFoundException(id));
-  // }
-
-  // @PutMapping("/cursos/{id}")
-  // Curso replaceCurso(@RequestBody Curso newCurso, @PathVariable Long id) {
-
-  //   return repository.findById(id)
-  //     .map(curso -> {
-  //       curso.setTitulo(newCurso.getTitulo());
-  //       curso.setCargaHoraria(newCurso.getCargaHoraria());
-	// 			curso.setDataInicio(newCurso.getDataInicio());
-	// 			curso.setVideoaulas(newCurso.getVideoaulas());
-  //       return repository.save(curso);
-  //     })
-  //     .orElseGet(() -> {
-  //       newCurso.setId(id);
-  //       return repository.save(newCurso);
-  //     });
-  // }
-
-  // @DeleteMapping("/cursos/{id}")
-  // void deleteCurso(@PathVariable Long id) {
-  //   repository.deleteById(id);
-  // }
+  @PostMapping(
+    value="index_row",
+    consumes="application/json"  
+  )
+  String newIndexRow(@RequestBody Curso curso) {
+    IndexRow element = new IndexRow(curso);
+    return element.toString();
+  }
 }
